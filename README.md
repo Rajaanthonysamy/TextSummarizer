9399e442916b7e88de2dd167e3d31e4442b381c1
A.Raja <rajaanthonysamy5798@gmail.com>
2025-06-17 21:22:08 +0530

    data ingestion first step completed

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Config.yaml
+2. params.yaml
+3. Config entity
+4. Configuration entity
+5. Update the components- Data Ingestion, Data transformation, Model Trainer
+6. Create our pipeline-- Training Pipeline, Prediction pipeline
+7. Front end-- API's, Training APIs , Batch prediction APIS
--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -7,35 +7,42 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """
-    Reads a YAML file and returns its content as a ConfigBox object.
-    
+    """reads yaml file and returns
+
     Args:
-        path_to_yaml (Path): The path to the YAML file.
-        
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
     Returns:
-        ConfigBox: The content of the YAML file as a ConfigBox object.
+        ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml, 'r') as yaml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logger.debug(f"yaml file content: {content}")
             return ConfigBox(content)
-    except BoxValueError as e:
-        logger.error(f"Error reading YAML file at {path_to_yaml}: {e}")
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
         raise e
     
 @ensure_annotations
-def create_directories(dirs: list,verbose= True) -> None:
-    """
-    Creates directories if they do not exist.
-    
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
     Args:
-        dirs (list): List of directory paths to create.
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
-    for dir_path in dirs:
-        os.makedirs(dir_path, exist_ok=True)
-        logger.info(f"Created directory: {dir_path}")
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"Directory already exists: {dir_path}")
+            logger.info(f"created directory at: {path}")
+
